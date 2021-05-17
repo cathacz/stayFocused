afa08c417a4811bc654abd244ada71207b067230
catha <catharinaczipf@hotmail.com>
2021-05-17 15:09:21 +0200

    sorry

---
+++ src/scripts/index.js
@@ -0,0 +1,13 @@
+// function (createTask) for "create task" to create new task in ul (ANGELOS)
+
+// function (timer) counting backwards – default 00:00
+// function (startTime) that sets time of task(OMAR)
+
+// function (stopTime) that stops task >> alert "are you sure? only _____ minutes left" (CATHA)
+
+// list items need buttons and timer
+
+// function (endDay) percentage of how much is been a accomplished (ALEX)
+
+// connect everything so it actually works :)
+// >> do that together – before merging!
