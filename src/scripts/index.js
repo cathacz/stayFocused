f5c5d0bef8347ea741270cb47d16e569f5d64e04
Omar <omar0940@gmail.com>
2021-05-17 20:51:37 +0200

    timer funtion Omar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,7 +2,27 @@
 
 // function (timer) counting backwards – default 00:00
 // function (startTime) that sets time of task(OMAR)
-const starttime = (e) => {};
+const startTimer = (duration, display) => {
+  let timer = duration,
+    minutes,
+    seconds;
+  setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    display.textContent = minutes + ":" + seconds;
+    if (--timer < 0) {
+      timer = duration;
+    }
+  }, 1000);
+};
+window.onload = function () {
+  let inputUser = 60 * 1,
+    display = document.querySelector("#time");
+  startTimer(inputUser, display);
+};
 // function (stopTime) that stops task >> alert "are you sure? only _____ minutes left" (CATHA)
 
 // list items need buttons and timer
