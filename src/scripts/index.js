bef92600c85a06ba326d93dffc49e9b1c8097e4f
catha <catharinaczipf@hotmail.com>
2021-05-20 11:52:42 +0200

    finial touches

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,33 +1,29 @@
-// function (createTask) for "create task" to create new task in ul (ANGELOS)
-
+// function (createTask) for "create task" to create new task in ul (----------ANGELOS----------)
 let kstart = "";
 let timing = "";
 let kstop = "";
 const createTask = (e) => {
   e.preventDefault();
   let myData = document.querySelector("input").value;
-
   if (myData != "") {
     let myTime = document.querySelector('input[type="range"]').value;
     getComputedStyle;
     let newTask = document.createElement("li");
     newTask.classList.add("classforall");
     let texti = document.createTextNode(myData);
-
     newTask.appendChild(texti);
-
     document.querySelector(".taskList").appendChild(newTask);
     document.querySelector("input").value = "";
-
     const newbox = document.createElement("div");
     newbox.classList.add("basics");
-    // for the balloon creation effect
+
+    // for the balloon creation effect --------------------------------------------------- BALLOON
     let balloon = document.createElement("div");
     balloon.classList.add("balloon");
     let balloonroom = document.querySelector(".displayTimer");
-
     balloonroom.appendChild(balloon);
-    // kstart button fireTheTimer
+
+    // kstart button fireTheTimer ---------------------------------------------------start button
     kstart = document.createElement("button");
     kstart.classList.add("start");
     kstart.addEventListener("click", (e) => {
@@ -35,36 +31,28 @@ const createTask = (e) => {
         0,
         2
       );
-
       inputUser = 60 * +mycurrenttime;
       display = document.querySelector("#time");
       clearInterval(interval);
-
       startTimer(inputUser, display);
     });
-    //our button for the stop
+    //our button for the stop ------------------------------------------------------stop button
     kstop = document.createElement("button");
     kstop.classList.add("toStopit");
     //our time list
     timing = document.createElement("div");
     timing.classList.add("task_time");
-
     timing.innerHTML = `${myTime} minutes`;
-
     kstart.innerHTML = "start";
     kstop.innerHTML = "stop";
-
     // Injecting the buttons into the our box
     newbox.appendChild(timing);
     newbox.appendChild(kstart);
     newbox.appendChild(kstop);
-
     newTask.appendChild(newbox);
     //kstop button , it deletes the task if thew user says yes
-
     const stopTime = () => {
       alert("Are you sure? There are only a few minutes left!");
-
       var strForStop = prompt(`Are you really sure? Please type "yes" or "no"`);
       let strToLowerCase = strForStop.toLowerCase();
       if (strToLowerCase.includes("yes")) {
@@ -89,19 +77,14 @@ const createTask = (e) => {
     document.querySelector("input").placeholder = "what do you wanna do next?";
   }
 };
-
 const keyCheck = (e) => {
   //console.log(event);
   if (e.key == "Enter") createTask(e);
 };
-
-document.querySelector("form").addEventListener("submit", createTask);
 document.querySelector("form").addEventListener("submit", createTask);
 
 // function (timer) counting backwards – default 00:00
-
-// function of input range for time input (Omar) (thanks to Roman)
-
+// function of input range for time input (Omar) (thanks to Roman) -------------------- OMAR
 let range = document.querySelector('input[type="range"]');
 let timeondisplay = document.querySelector("#time");
 let rangeValue = function () {
@@ -133,9 +116,7 @@ const startTimer = (duration, display) => {
       balloon.style.opacity = "100%";
       balloon.style.transition = "6s";
     }
-
   }, 1000);
-
 };
 let myTime = document.querySelector('input[type="range"]');
 let mycurrenttime;
@@ -143,22 +124,12 @@ let inputUser;
 document.querySelectorAll(".start").forEach((el) =>
   el.addEventListener("click", (e) => {
     console.log(e.target.previousElementSibling);
-    //console.log(5454);
-    // mycurrenttime = document.querySelector('input[type="range"]').value;
-    // inputUser = 60 * +mycurrenttime;
-    // display = document.querySelector("#time");
-    // clearInterval(interval);
-    // startTimer(inputUser, display);
   })
 );
 
-//list items need buttons and timer
-
-/* END DAY FUNCTION (ALEX) */
 //selectors
 const endBtn = document.querySelector(".endDay");
-
-//functions
+//functions -----------------------------------------------------------------------END DAY
 function endDay() {
   var str = prompt(`Are you a 100% sure? Please type "yes" or "no"`);
   let strToLowerCase = str.toLowerCase();
@@ -167,6 +138,8 @@ function endDay() {
     let ali = document.querySelectorAll("li");
     ali.forEach((el) => el.remove());
     let timetodelete = document.querySelector("#time");
+    timetodelete.remove();
+
     let balloondestroyer = document.querySelectorAll(".balloon");
     balloondestroyer.forEach((el) => el.remove());
   } else if (strToLowerCase.includes("no")) {
@@ -177,23 +150,5 @@ function endDay() {
     prompt(`Be so kind and type "yes" or "no"`);
   }
 }
-
 //events
 endBtn.addEventListener("click", endDay);
-// connect everything so it actually works :)
-// >> do that together – before merging!
-// :00 on the duration part
-//FIX THE STOP BUTTON TO SAY HOW MANY MINUTES LEFT AND MAKE IT STOP THE TASK
-
-//AFTER THE END DAY TASK SUMM THE AMM OF MINUTES
-
-// maybe next life
-// var colors = [
-//   "$field-color41",
-//  $field-color42 ``,
-//   "$field-color43",
-//   "$field-color44",
-//   "$field-color45",
-// ];
-// var random_color = colors[Math.floor(Math.random() * colors.length)];
-// document.querySelector(".balloon").style.color = random_color;
